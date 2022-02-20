d74d74aaf1e0e51cdf6f533fb047752b9cf3a07f
RichardByrne95 <richardbyrne195@gmail.com>
2022-02-20 21:07:35 +0000

    Add hello world print

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main() {
+    char helloworld[] = "Hello World!";
     char name[20];
+
     printf("Enter your name: ");
     fgets(name, 20, stdin);
     printf("Your name is %s", name);
+
+    printf("%s", helloworld);
 }
